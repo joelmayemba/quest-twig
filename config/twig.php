cc48d5e6ca4e93409a418e06e5b1e42a5785c19a
Joël MAYEMBA <joelmayemba@gmail.com>
2024-01-22 00:45:45 +0100

    up

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+//On ajoute la globale author à l'initialisation de l'objet Twig
+$author = 'Joël Wilder';
+$twig->addGlobal('author', $author);
--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
-
+require_once __DIR__ . '/../src/View/navbar.html.twig';
 $name = 'Wilder';
 
 echo $twig->render('home.html.twig', ['name' => $name]);
